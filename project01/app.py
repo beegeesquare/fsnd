adb4cdf1767f5afb549ed5bfd8fb7b08b4d447bb
Balagangadhar Bathula <bb4341@att.com>
2020-01-29 15:36:17 -0500

    Added code for posting artist edited form and get method for venue edit form

--- project01/app.py
+++ project01/app.py
@@ -380,7 +380,7 @@ def edit_artist_submission(artist_id):
     # artist record with ID <artist_id> using the new attributes
     artist = Artist.query.get(artist_id)
     data = request.form
-    print(artist)
+    # print(artist)
     if artist:  # Means that it's not none type, that means there is
         try:
             # Convert the seeking_talent to boolean
@@ -416,28 +416,31 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
     form = VenueForm()
-    venue = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    }
-    # TODO: populate form with values from venue with ID <venue_id>
-    return render_template('forms/edit_venue.html', form=form, venue=venue)
+    # populate form with values from venue with ID <venue_id>
+    venue = Venue.query.get(venue_id)
+    if venue:  # Means the venue id exists in the database
+        form.name.data = venue.name
+        form.city.data = venue.city
+        form.facebook_link.data = venue.facebook_link
+        form.state.data = venue.state
+        form.phone.data = venue.phone
+        form.website.data = venue.website
+        form.image_link.data = venue.image_link
+        if venue.seeking_talent:
+            form.seeking_talent.data = venue.seeking_talent
+            form.seeking_description.data = venue.seeking_description
+        return render_template('forms/edit_venue.html', form=form, venue=venue)
+    else:
+        flash('Venue ' + str(venue_id) + ' not found in the database')
+        return render_template('pages/home.html')
+
 
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
     # TODO: take values from the form submitted, and update existing
     # venue record with ID <venue_id> using the new attributes
+    
     return redirect(url_for('show_venue', venue_id=venue_id))
 
 
