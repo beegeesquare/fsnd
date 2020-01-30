0133a571b8f42b1e9b2dcf5b0eccbcc47644b55c
Balagangadhar Bathula <bb4341@att.com>
2020-01-30 07:31:11 -0500

    Rendering data from the database for show

--- project01/app.py
+++ project01/app.py
@@ -96,7 +96,7 @@ class Show(db.Model):
     __tablename__ = 'Show'
 
     show_id = db.Column(db.Integer, primary_key=True)  # This make nullable False
-    artist_image_link = db.Column(db.String(150))
+    # artist_image_link = db.Column(db.String(150)) # This should come from artist table (dropped it in the database)
     start_time = db.Column(db.String(100))
 
     # Now add the Foreign keys for the Venue and Artist
@@ -526,45 +526,32 @@ def create_artist_submission():
 @app.route('/shows')
 def shows():
     # displays list of shows at /shows
-    # TODO: replace with real venues data.
+    # replace with real venues data.
     #       num_shows should be aggregated based on number of upcoming shows per venue.
-    data = [{
-        "venue_id": 1,
-        "venue_name": "The Musical Hop",
-        "artist_id": 4,
-        "artist_name": "Guns N Petals",
-        "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-        "start_time": "2019-05-21T21:30:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 5,
-        "artist_name": "Matt Quevedo",
-        "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-        "start_time": "2019-06-15T23:00:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 6,
-        "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "start_time": "2035-04-01T20:00:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 6,
-        "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "start_time": "2035-04-08T20:00:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 6,
-        "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "start_time": "2035-04-15T20:00:00.000Z"
-    }]
-    
+    """
+    select
+    venue_id,
+    artist_id,
+    "Venue".name venue_name,
+    "Artist".name artist_name,
+    "Artist".image_link artist_image_link
+    from
+    "Show"
+    inner join "Venue" on ("Show".venue_id = "Venue".id)
+    left  join "Artist" on ("Show".artist_id = "Artist".id);
+    """
+    # This is 3 tuple with (Show, Artist, Venue) objects
+    show_query = db.session.query(Show, Artist, Venue).filter(Show.artist_id ==
+                                                              Artist.id).filter(Show.venue_id==Venue.id).all()
+    data = [
+            {"venue_id": sav[2].id, "venue_name": sav[2].name,
+             "artist_id":sav[1].id, "artist_name": sav[1].name,
+             "artist_image_link": sav[1].image_link,
+             "start_time": sav[0].start_time
+             }
+            for sav in show_query
+            ]
+    # print(data)
     return render_template('pages/shows.html', shows=data)
 
 
